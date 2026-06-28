2233e2ae8c9ab7bbb3542de3aa6b28003543cb5e
morishun045 <morito.shun8051@mail.kyutech.jp>
2026-06-28 19:17:02 +0900

    feature: 詳細ページの作成

---
+++ app/[id]/page.tsx
@@ -0,0 +1,11 @@
+type DetailProps = {
+    params: {id: string }
+}
+
+export default async function DetailPage({params}: DetailProps){
+    return(
+        <div>
+            
+        </div>
+    )
+}

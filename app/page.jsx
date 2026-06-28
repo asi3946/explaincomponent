3a42831bd3616de14ad87478bfe40a43993b33d9
asi3946 <yuuma218eldol@gmail.com>
2026-06-28 18:36:23 +0900

    fix: pageから渡すidをstringに変更 #5

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,9 @@
 import CardList from "@/component/CardList";
 
 const users = [
-  { id: 1, name: "田中 太郎", comment: "数学が好きです" },
-  { id: 2, name: "田中 二郎", comment: "ラーメンが好きです" },
-  { id: 3, name: "田中 三郎", comment: "三河屋で働いています" },
+  { id: "1", name: "田中 太郎", comment: "数学が好きです" },
+  { id: "2", name: "田中 二郎", comment: "ラーメンが好きです" },
+  { id: "3", name: "田中 三郎", comment: "三河屋で働いています" },
 ];
 
 export default function Home() {

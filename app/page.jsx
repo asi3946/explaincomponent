33276d2ccbb7e7c791edd25ecd8e22f5941efaf9
asi3946 <yuuma218eldol@gmail.com>
2026-06-28 17:53:06 +0900

    update: 自己紹介カード複数化 #5

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,10 @@
 import UserCard from "../component/UserCard";
 export default function Home() {
-  return <UserCard name="田中 太郎" comment="数学が好きです" />;
+  return (
+    <div>
+      <UserCard name="田中 太郎" comment="数学が好きです" />
+      <UserCard name="田中 二郎" comment="ラーメンが好きです" />
+      <UserCard name="田中 三郎" comment="三河屋で働いています" />
+    </div>
+  );
 }

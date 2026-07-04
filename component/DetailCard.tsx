a0c3a5768f8c20cc9a4c129e9a67f7fd7f4bc7cb
morishun045 <morito.shun8051@mail.kyutech.jp>
2026-07-04 16:30:37 +0900

    ux: css記述

--- component/DetailCard.tsx
+++ component/DetailCard.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 type UserDetailProps = {
     image: string
     name: string 
@@ -6,15 +8,15 @@ type UserDetailProps = {
 
 export default function DetailCard ({ image, name, comment }:UserDetailProps){
     return(
-        <div>
-            <div>
-                {name}
+        <div className="border-4 p-4 rounded-xl">
+            <div className="flex border-b-4 p-4 mb-4 justify-center">
+                <span className="text-2xl">{name}</span>
             </div>
-            <div>
-                <div>
-                    <img src={image} />
+            <div className="flex">
+                <div className="mr-4 border-4 rounded-xl  overflow-hidden">
+                    <Image src={image} alt={name} width={300} height={150} className="w-full h-full object-cover" />
                 </div>
-                <div>
+                <div className="border-4 p-4 rounded-xl right-0">
                     <span>{comment}</span>
                 </div>
             </div>
